9e8fff879d65427287cfc72365fe164f0b5bf9ae
Ed Bartholomew <edmbartholomew@gmail.com>
2016-07-11 08:35:56 -0400

    Initial checkin

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'opsworks-redis-cluster'
+maintainer       'Ed Bartholomew'
+maintainer_email ''
+license          'All rights reserved'
+description      'Installs/Configures opsworks-redis-cluster'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
